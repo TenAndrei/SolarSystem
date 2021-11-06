5189976c3b84a6b2a021e763e3994f8c5ae539df
TenAndrei <fcbfanat@gmail.com>
2021-11-06 10:56:34 -0400

    SolarSystemBasicStructure

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem solarSystem=new SolarSystem();
+
+        solarSystem.planets=2;
+        solarSystem.Sun=1;
+        solarSystem.moon=1;
+
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SolarSystem {
+
+    int Sun;
+    int planets;
+    int moon;
+
+}
