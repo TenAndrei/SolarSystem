fee71989aead6b696a67f75e88236e6cfb828e79
TenAndrei <fcbfanat@gmail.com>
2021-11-06 11:54:15 -0400

    feature sun added with a bug that needs to be fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,12 @@ public class Main {
         solarSystem.Sun=1;
         solarSystem.moon=1;
 
-    }
+        featuresSun featuresSun=new featuresSun();
+        featuresSun.color="yellow";
+        featuresSun.heat="6000";
+        featuresSun.temp="5cel";
+        featuresSun.radius=12;
+
+
+  }
 }
---
+++ src/com/company/featuresSun.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class featuresSun {
+
+    String heat;
+    String temp;
+    String radius;
+    String color;
+    String marital;
+}
